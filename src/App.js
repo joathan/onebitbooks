ce3970446a3fe5121b18baed780ea42d7332e7a9
Joathan Francisco <joathanf@gmail.com>
2020-05-06 01:48:23 -0300

    Configurando o layout - Header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Routes from "./routes";
+import Header from "./components/Header";
 
 function App() {
  return (
    <BrowserRouter>
+     <Header/>
      <Routes />
    </BrowserRouter>
  );
